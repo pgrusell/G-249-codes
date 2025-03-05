31310a4e1729474e201a883b14364442612112bc
pgrusell <pgrusell@MacBook-Pro-de-Pablo.local>
2025-03-05 14:23:38 +0100

    Optimization of the Python program for the NN reconstruction.

--- ana/reco/nnreco.py
+++ ana/reco/nnreco.py
@@ -12,77 +12,33 @@ $ python file_in file_out model_path
             training and only the 20% will be displayed as a result.
 '''
 
-import numpy as np
-import pandas as pd
 import sys
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from tensorflow import keras
-from tensorflow.keras import layers
+from utils import VertexReconstruction
 
-# ------ Data access --------
-file_in = sys.argv[1]
-data = pd.read_csv(file_in, delim_whitespace=True, header=None)
 
-# Source file must have the form
-# x1 y1 z1 ... x4 y4 z4 z_vertex_sim
-X = data.iloc[:, :12].values
-y = data.iloc[:, 12].values
+def main():
 
-# ------ Model -----------
-model_path = sys.argv[3] if len(sys.argv) == 4 else None
+    # Get the file inputs
+    file_in = sys.argv[1]
+    file_out = sys.argv[2]
+    model_path = sys.argv[3] if len(sys.argv) == 4 else None
 
-# If a trained NN is provided, use it to reconstruct the vertex
-if model_path:
-    model = keras.models.load_model(model_path, compile=False)
-    model.compile(optimizer='adam', loss='mse')
+    # Create an object from utils.py
+    vertex_reconstructor = VertexReconstruction(file_in)
 
-    # Normalizing the input data with StandardScaler
-    scaler = StandardScaler()
-    # Assuming model was trained with normalized data
-    X = scaler.fit_transform(X)
+    # If a model has been provided, we loaded it
+    if model_path:
+        vertex_reconstructor.load_model(model_path)
 
-    # Predict over the whole set of data
-    y_predict = model.predict(X).flatten()
-    y_real = y
+    # Predict the results from the input file
+    # Notice that if no model is loaded this step will
+    # train a new one, but the predicted results will be decreased
+    # in a 80 %.
+    y_predicted, y_real = vertex_reconstructor.predict()
 
-# If no model is provided, train a new one
-else:
-    # ------- Data modification ---------
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42)
+    # Store the info in the output file
+    vertex_reconstructor.save_results(file_out, y_predicted, y_real)
 
-    # Data normalization
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
 
-    # Defining a more complex neural network
-    model = keras.Sequential([
-        layers.Dense(256, activation='relu', input_shape=(12,)),
-        layers.Dense(512, activation='relu'),
-        layers.Dense(256, activation='relu'),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(32, activation='relu'),
-        layers.Dense(1)
-    ])
-
-    model.compile(optimizer='adam', loss='mse')
-
-    # Training
-    model.fit(X_train, y_train, epochs=100,
-              batch_size=32, validation_split=0.1)
-
-    # Save the model
-    model.save('model.h5')
-
-    # Predict only for the test set
-    y_predict = model.predict(X_test).flatten()
-    y_real = y_test
-
-# -------- Real output ---------
-file_out = sys.argv[2]
-with open(file_out, "w") as f:
-    for pred, real in zip(y_predict, y_real):
-        f.write(f'{pred} {real}\n')
+if __name__ == "__main__":
+    main()
---
+++ ana/reco/utils.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+class VertexReconstruction:
+
+    def __init__(self, file_in: str):
+        self.file_in = file_in
+        self.data = self.load_data()
+        self.X, self.y = self.prepare_data()
+        self.scaler = StandardScaler()
+        self.model = None
+
+    def load_data(self) -> pd.DataFrame:
+        '''This function loads the data from the source file.
+        The data structure must be something like
+        x1 y1 z1 ... x4 y4 z4 z_vertex_sim'''
+        return pd.read_csv(self.file_in, delimiter=' ', header=None)
+
+    def prepare_data(self):
+        '''This function Transforms the data to take the four 3D points 
+        as input and the simulated z-vertex as output.'''
+        X = self.data.iloc[:, :12].values
+        y = self.data.iloc[:, 12].values
+        return X, y
+
+    def load_model(self, model_path: str):
+        '''This function loads a model given its path (it has to point
+        to a .h5 file).'''
+        self.model = keras.models.load_model(model_path, compile=False)
+        self.model.compile(optimizer='adam', loss='mse')
+
+        # The model has been trained with scaled data so we have to do the same
+        self.X = self.scaler.fit_transform(self.X)
+
+        # Predict over the whole set of data
+        self.y_predicted = self.model.predict(self.X).flatten()
+        self.y_real = self.y
+
+    def train_model(self):
+        '''This function will take X and y data an use it to train a NN.
+        Only the 20% of the data will be used for testing, and therefore
+        only this part of the data will be presented as the result'''
+        # Split the data in train and test sets
+        X_train, X_test, y_train, y_test = train_test_split(
+            self.X, self.y, test_size=0.2, random_state=42)
+
+        # Scale the data
+        X_train = self.scaler.fit_transform(X_train)
+        X_test = self.scaler.transform(X_test)
+
+        # Create a new model
+        self.model = keras.Sequential([
+            layers.Dense(256, activation='relu', input_shape=(12,)),
+            layers.Dense(512, activation='relu'),
+            layers.Dense(256, activation='relu'),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(64, activation='relu'),
+            layers.Dense(32, activation='relu'),
+            layers.Dense(1)
+        ])
+        self.model.compile(optimizer='adam', loss='mse')
+
+        # Training
+        self.model.fit(X_train, y_train, epochs=100,
+                       batch_size=32, validation_split=0.1)
+
+        # Save the model in the current directory
+        self.model.save("model.h5")
+
+        # Predict only for the test set
+        self.y_predicted = self.model.predict(X_test).flatten()
+        self.y_real = y_test
+
+    def predict(self):
+        '''This function returns the result of the vertex reconstruction
+        i.e. the vector with the predicted data and the vector with the
+        real data.'''
+        if self.model is None:
+            print("No model has been loaded yet. A new one is going to be trained:\n")
+            self.train_model()
+
+        return self.y_predicted, self.y_real
+
+    @staticmethod
+    def save_results(file_out: str, y_predicted, y_real):
+        """Save the predicted and real vertex values to a file."""
+        with open(file_out, "w") as f:
+            for zrec, zsim in zip(y_predicted, y_real):
+                f.write(f'{zrec} {zsim}\n')
